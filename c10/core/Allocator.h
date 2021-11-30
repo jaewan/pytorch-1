5891987d3f28bea077a7f87121dadee36bf3a884
Ubuntu <ubuntu@ip-172-31-33-236.us-east-2.compute.internal>
2021-11-30 06:22:00 +0000

    modified allocator.h due to build error

--- c10/core/Allocator.h
+++ c10/core/Allocator.h
@@ -151,7 +151,7 @@ inline bool operator!=(std::nullptr_t, const DataPtr& dp) noexcept {
 struct C10_API Allocator {
   virtual ~Allocator() = default;
 
-  virtual DataPtr allocate(size_t n) const = 0;
+  virtual DataPtr allocate(size_t n, bool weightEmpty=false) const = 0;
 
   // If this returns a non nullptr, it means that allocate()
   // is guaranteed to return a unique_ptr with this deleter attached;
